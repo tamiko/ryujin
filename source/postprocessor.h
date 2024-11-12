79ea22440b78a56f59095ec10a66028eee515666
Matthias Maier <tamiko@43-1.org>
2024-11-11 18:40:20 -0600

    Postprocessor: use MPIEnsemble

--- source/postprocessor.h
+++ source/postprocessor.h
@@ -5,8 +5,9 @@
 
 #pragma once
 
-#include <compile_time_options.h>
-#include <offline_data.h>
+#include "compile_time_options.h"
+#include "mpi_ensemble.h"
+#include "offline_data.h"
 
 #include <deal.II/base/parameter_acceptor.h>
 #include <deal.II/base/smartpointer.h>
@@ -73,7 +74,7 @@ namespace ryujin
     /**
      * Constructor.
      */
-    Postprocessor(const MPI_Comm &mpi_communicator,
+    Postprocessor(const MPIEnsemble &mpi_ensemble,
                   const OfflineData<dim, Number> &offline_data,
                   const HyperbolicSystem &hyperbolic_system,
                   const ParabolicSystem &parabolic_system,
@@ -153,7 +154,7 @@ namespace ryujin
      */
     //@{
 
-    const MPI_Comm &mpi_communicator_;
+    const MPIEnsemble &mpi_ensemble_;
 
     dealii::SmartPointer<const OfflineData<dim, Number>> offline_data_;
     dealii::SmartPointer<const HyperbolicSystem> hyperbolic_system_;
